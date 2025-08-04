36f4af299f67c93989e1599ffb92f15a547996b8
Haoyu Wang <hwan0328@student.monash.edu>
2025-08-04 22:10:35 +1000

    Initial commit

---
+++ fit5032/src/components/haoyu.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <div class="greetings">
+        <h3>
+            Hello Haoyu Wang.
+        </h3>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+}
+
+h3 {
+    font-size: 1.2rem;
+    color: orange;
+}
+</style>
